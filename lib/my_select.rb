3e5940a35d5208b19592b3504abccb5adb376902
Rani Shah <rani76@gmail.com>
2018-02-27 01:07:29 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,7 @@
 def my_select(collection)
-   i = 0 
+   i = 0
   new_array = []
-    while i < collection.length 
+    while i < collection.length
       if yield(collection[i]) === true
     new_array << collection[i]
     end
